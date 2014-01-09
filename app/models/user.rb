4f2b073d2b2e8acb2a6a0a86cdc5b9a3117fcd41
Daniel Åsljung <daniel.asljung@gmail.com>
2014-01-09 17:00:30 +0100

    Add and modify files from sample app for user functionaility

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+  before_create :create_remember_token
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+  															uniqueness: { case_sensitive: false }
+  has_secure_password 
+  validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+   def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+end
