d819fdd54adb46b36620a8e0b114ab27b53491c6
Daniel Åsljung <daniel.asljung@gmail.com>
2014-01-31 12:00:40 +0100

    Implement ckeditor for idea creation and edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require ckeditor/init
+//= require ckeditor/config
 //= require bootsy
 //= require bootstrap
 //= require turbolinks
---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,10 @@
+CKEDITOR.config.toolbar_mini =
+    [
+        ['Format'],
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['NumberedList','BulletedList','-','Outdent','Indent'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink'],
+        ['Image','Table','HorizontalRule']  
+    ];
