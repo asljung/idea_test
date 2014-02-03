3852f3ba5fc869bcc9d4974fdf0495cf33640e93
Daniel Åsljung <daniel.asljung@gmail.com>
2014-02-03 10:45:02 +0100

    Change to compile=true in prduction.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ IdeaTest::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,6 +77,6 @@ IdeaTest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   config.assets.precompile += Ckeditor.assets
 end
